78e0535bf5f735206bd946d65393fe76f2a48906
Alex Dusenbery <adusenbery@edx.org>
2022-02-15 14:49:41 -0500

    use a container for the whole UI

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ function App() {
   const [currentSong, setCurrentSong] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   //let [allSongs, setAllSongs] = useState(null);
-  const [allPerformances, setAllPerformances] = useState([])
+  const [allPerformances, setAllPerformances] = useState({});
 
   useEffect(() => {
     const fetchPerformances = () => {
@@ -27,50 +27,57 @@ function App() {
       });
     };
     fetchPerformances();
-  }, [setAllPerformances])
+  }, [setAllPerformances]);
 
   const renderSongs = function(performance) {
     
-  }
+  };
+
+  const performanceCard = (performance) => {
+    return (
+      <Col lg={`auto`}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>{performance.metadata.title}</Card.Title>
+            <Button onClick={() => renderSongs(performance)}>
+              {`Show songs`}
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    );
+  };
 
   return (
     // React.Fragment so that we don't need adjacent elements inside a wrapping div.
     <React.Fragment>
-      <Toast show={isLoading} onClose={() => setIsLoading(false)}>
-        <Toast.Header>
-          <strong className="mr-auto">{`Loading...`}</strong>
-        </Toast.Header>
-        <Toast.Body>{`Loading the data`}</Toast.Body>
-      </Toast>
-
-      {allPerformances?.length && (
-        <Container>
-          <CardGroup>
-          {allPerformances.map(performance => {
-            return (
-              <Col lg={`auto`}>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>{performance.metadata.title}</Card.Title>
-                    <Button onClick={() => renderSongs(performance)}>
-                      {`Show songs`}
-                    </Button>
-                  </Card.Body>
-                </Card>
-              </Col>
-            )
-          })
-          }
-          </CardGroup>
-          <Row>
+      <Container>
+        <Row>
+          <Col>
+            <Toast show={isLoading} onClose={() => setIsLoading(false)}>
+              <Toast.Header>
+                <strong className="mr-auto">{`Loading performances...`}</strong>
+              </Toast.Header>
+              <Toast.Body>{`Loading performances...`}</Toast.Body>
+            </Toast>
+            {allPerformances?.length && (
+              <Container>
+                <CardGroup>
+                  {allPerformances.map(performance => performanceCard(performance))}
+                </CardGroup>
+              </Container>
+            )}
+          </Col>
+      
+          <Col>
             <ReactAudioPlayer
               src={currentSong}
-              controls  
+              controls
             />
-          </Row>
-        </Container>
-      )}
+          </Col>
+        </Row>
+      </Container>
     </React.Fragment>
   );
 }
